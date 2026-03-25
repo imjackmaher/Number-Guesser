50b3e1c2aab1a0c989e803f381a0964e6b805b85
lilj12292 <jack.maher.iii@icloud.com>
2026-03-25 14:17:01 -0400

    notes about code in JS

--- java.js
+++ java.js
@@ -1,1 +1,17 @@
-console.log(`hello`);
+// THIS IS HOW TO MAKE CONSOLE TEXT - console.log(`hello`);
+// WINDOWED ALERTS - window.alert(`67`);
+/* INPUT TEXT FOR ELEMENTS
+document.getElementById("h1").textContent = `hello`;
+document.getElementById("p1").textContent = `hello`;
+*/
+/*Variable = stores value - behaves as if it were the value
+1. declaration - let x;
+2. assignment - x = 100;
+3. both together - let x = 123;
+4. insert variable in text -
+let age = 15;
+console.log(`you are ${age} years old.`);
+5. display datatypes - console.log(typeof age);
+6. string - let x = "letters and numbers but no math";
+7. boolean - true or false - let x = true;
+*/
