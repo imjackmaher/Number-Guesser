e80cbed1c6f457883bd8cca2abfde793f2be0a17
lilj12292 <jack.maher.iii@icloud.com>
2026-03-26 09:45:27 -0400

    Notes On More JS

--- java.js
+++ java.js
@@ -1,10 +1,12 @@
-// THIS IS HOW TO MAKE CONSOLE TEXT - console.log(`hello`);
-// WINDOWED ALERTS - window.alert(`67`);
-/* INPUT TEXT FOR ELEMENTS
+/* Console Text and Window Alerts
+THIS IS HOW TO MAKE CONSOLE TEXT - console.log(`hello`);
+WINDOWED ALERTS - window.alert(`67`);
+*/
+/* Input for Text Elements
 document.getElementById("h1").textContent = `hello`;
 document.getElementById("p1").textContent = `hello`;
 */
-/*Variable = stores value - behaves as if it were the value
+/*Variables
 1. declaration - let x;
 2. assignment - x = 100;
 3. both together - let x = 123;
@@ -14,4 +16,31 @@ console.log(`you are ${age} years old.`);
 5. display datatypes - console.log(typeof age);
 6. string - let x = "letters and numbers but no math";
 7. boolean - true or false - let x = true;
+*/
+/* Example Of Variables
+let fullName = "Jack M";
+let age = 15;
+let student = true;
+
+document.getElementById("p1").textContent = `Your Name Is ${fullName}`;
+document.getElementById("p2").textContent = `You Are ${age} Years Old`;
+document.getElementById("p3").textContent = `You Are A Student: ${student}`;
+*/
+/* Math Equation Basic
+addition is +
+subtraction is -
+multiplication is *
+division is /
+exponential is **
+modulus (show remainder) is %
+example:
+let students = 30;
+students = students / 2 - this would give a result of 15
+students = students % 2 - this would give a result of 0
+*/
+/* Augmented Assignment Operators
+let number = 30;
+number += 1; - result is 31
+number *= 2; - result is 60
+this can be applied with ALL math functions (+ - * / % etc.)
 */
